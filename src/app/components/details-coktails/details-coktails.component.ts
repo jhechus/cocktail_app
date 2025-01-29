5195a03ffb0f3d2db4567d81000435a261774a63
jhechus <jess.osm1412@gmail.com>
2025-01-29 14:54:37 -0600

    parametros de ruta

--- src/app/components/details-coktails/details-coktails.component.ts
+++ src/app/components/details-coktails/details-coktails.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-details-coktails',
   imports: [],
   templateUrl: './details-coktails.component.html',
-  styleUrl: './details-coktails.component.scss'
+  styleUrl: './details-coktails.component.scss',
 })
 export class DetailsCoktailsComponent {
+  /** Inyección del servicio `ActivatedRoute` para obtener los parámetros de la URL.
+    - `ActivatedRoute` proviene de `@angular/router`.
+    - Se usa `inject(ActivatedRoute)` en lugar del constructor para mayor eficiencia.
+    - Permite capturar el parámetro `id` que identifica el cóctel seleccionado.*/
+  private activateRoute = inject(ActivatedRoute);
 
+  ngOnInit() {
+    /** Suscripción a los parámetros de la URL.
+
+      - `this.activateRoute.params`: Observa los cambios en los parámetros de la ruta.
+      - `pipe(first())`: Obtiene solo el primer valor y completa la suscripción.
+      - `subscribe((params: Params) => {...})`: Captura el parámetro `id` de la URL.
+
+      🔹 `id` será el identificador del cóctel seleccionado y se usará para cargar sus detalles. */
+    this.activateRoute.params.pipe(first()).subscribe({
+      next: (params: Params) => {
+        const id = params['id']; // Extrae el ID de la URL
+        console.log(id); // Muestra el ID en la consola (para prueba
+      },
+    });
+  }
 }
